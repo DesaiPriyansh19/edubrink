ffb23942816df7d308ce26628d67ccfce2fce62d
shivamlogicray <m.shivam829@gmail.com>
2024-12-14 18:16:42 +0530

    AddedBySHIvam-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import "./App.css";
-
 import NavBar from "./components/NavBar";
 import HomePage from "./components/Home/HomePage";
 import AuthModal from "./components/AuthModal";
-
+import AboutPage from "./components/AboutUs/About";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="h-full m-0 p-0 w-full  ">
-       <button
+      <button
         onClick={() => setIsModalOpen(true)}
         className="px-4 py-2 bg-blue-500 text-white rounded-lg"
       >
@@ -19,19 +17,13 @@ function App() {
       </button>
 
       {/* Modal Component */}
-      <AuthModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      />
+      <AuthModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
 
-    
-   <NavBar/>
- <HomePage/>
-
+      <NavBar />
+      <HomePage />
+      <AboutPage />
     </div>
   );
 }
-
-
 
 export default App;
