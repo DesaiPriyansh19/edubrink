29a744ee4892c61e3d6a404aa799a5a021501434
karan <jangdekaran54@gmail.com>
2024-12-14 10:13:06 +0530

    googleLinkUpdated

--- utils/GoogleButton.jsx
+++ utils/GoogleButton.jsx
@@ -11,7 +11,7 @@ export default function GoogleButton() {
   const googleRespone = async (authResult) => {
     try {
       const response = await axios.get(
-        `http://localhost:4000/api/google/auth/google?code=${authResult["code"]}`
+        `https://edu-brink-backend.vercel.app/api/google/auth/google?code=${authResult["code"]}`
       );
 
       if (response.data && response.data.data.user) {
