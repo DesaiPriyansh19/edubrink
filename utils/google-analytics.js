4a0b64bf25c0dbd21091eb85a955c414fdcdf98d
wreaker-cpu <jangdekaran54@gmail.com>
2025-02-11 10:12:08 +0530

    Update google-analytics.js

--- utils/google-analytics.js
+++ utils/google-analytics.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga4";
 
-const TRACKING_ID = "G-XXXXXXXXXX"; // Replace with your Measurement ID
+const TRACKING_ID = import.meta.env.VITE_MEASUREMENT_ID; // Replace with your Measurement ID
 ReactGA.initialize(TRACKING_ID);
 
 export const trackPageView = (path) => {
