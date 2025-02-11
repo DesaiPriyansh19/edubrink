b3028876813e00488a835d33811ba9b3163ffc9b
wreaker-cpu <jangdekaran54@gmail.com>
2025-02-11 19:48:52 +0530

    Update google-analytics.js

--- utils/google-analytics.js
+++ utils/google-analytics.js
@@ -1,9 +1,10 @@
 import ReactGA from "react-ga4";
 
-const TRACKING_ID = import.meta.env.VITE_MEASUREMENT_ID; // Replace with your Measurement ID
+// const TRACKING_ID = import.meta.env.VITE_MEASUREMENT_ID; // Replace with your Measurement ID
+const TRACKING_ID = "G-ZHSLDD53DK"; // Replace with your Measurement ID
 ReactGA.initialize(TRACKING_ID);
 
 export const trackPageView = (path) => {
-    console.log("Tracking GA4 Page:", path); // Debugging log
-    ReactGA.send({ hitType: "pageview", page: path });
-  };
+  console.log("Tracking GA4 Page:", path); // Debugging log
+  ReactGA.send({ hitType: "pageview", page: path });
+};
