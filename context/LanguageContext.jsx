fd1ba1f984de0b7b817752096ed59710feb18077
Desai Priyansh <priyanshdesai62@gmail.com>
2025-03-29 23:47:15 +0530

    tejawal fonts added for arabic language

--- context/LanguageContext.jsx
+++ context/LanguageContext.jsx
@@ -1,22 +1,17 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import i18n from "../src/i18n";
-import { useNavigate } from "react-router-dom";
 
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState(
-    localStorage.getItem("language") || "en"
-  );
+  const [language, setLanguage] = useState(localStorage.getItem("language") || "en");
 
   useEffect(() => {
     i18n.changeLanguage(language);
     localStorage.setItem("language", language);
 
-    document.documentElement.setAttribute(
-      "dir",
-      language === "ar" ? "rtl" : "ltr"
-    );
+    document.documentElement.setAttribute("dir", language === "ar" ? "rtl" : "ltr");
+    document.documentElement.setAttribute("lang", language);  // ✅ This line ensures `<html lang="ar">`
   }, [language]);
 
   return (
